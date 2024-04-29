c8a8cf88265251e2079fd538eb1866f579c5e572
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2024-04-28 22:49:29 -0300

    feat: Initial configs for unit tests

---
+++ test/domain/users/users.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Test saveUsers', () => {
+	it('shoul throw exception when incorrect params sent', () => {
+		expect(1 + 1).toEqual(2)
+	})
+})
